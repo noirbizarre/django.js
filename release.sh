87a0749fe75c3912677edcb5e5f609cf58216341
Axel Haustant <noirbizarre@gmail.com>
2012-11-02 14:12:52 +0100

    Handle CHANGELOG in release.sh

--- release.sh
+++ release.sh
@@ -5,16 +5,21 @@
 # Build a source distrbution and upload on PyPI
 # Update version for new develpoment cycle
 VERSION_FILE=djangojs/__init__.py
+CHANGELOG_FILE=CHANGELOG.rst
+CHANGELOG_CURRENT='Current\n-------'
 
 CURRENT=$(grep __version__ $VERSION_FILE | sed "s/__version__ = '\(.*\)'/\1/")
 echo -n "Current version is $CURRENT, what version do you want to release ? "
 read RELEASE
 
+CHANGELOG_VERSION="$RELEASE ($(date +%Y-%m-%d))"
+SEP=$( printf "%${#CHANGELOG_VERSION}s" | tr " " "-" )
 
 python setup.py clean
 rm -rf *egg-info build dist
 sed -i "s/$CURRENT/$RELEASE/" $VERSION_FILE
-git commit $VERSION_FILE -m "Bump version $RELEASE"
+sed -i "1!N; s/$CHANGELOG_CURRENT/$CHANGELOG_VERSION\n$SEP/" $CHANGELOG_FILE
+git commit $VERSION_FILE $CHANGELOG_FILE -m "Bump version $RELEASE"
 git tag $RELEASE
 python setup.py register sdist upload
 
@@ -22,7 +27,8 @@ echo -n "Version $RELEASE released, what version do you want for next developmen
 read NEXT
 
 sed -i "s/$RELEASE/$NEXT/" $VERSION_FILE
-git commit $VERSION_FILE -m "Updated to version $NEXT for next development cycle"
+sed -i "1!N; s/$CHANGELOG_VERSION/$CHANGELOG_CURRENT\n\n- nothing yet\n\n\n$CHANGELOG_VERSION/" $CHANGELOG_FILE
+git commit $VERSION_FILE $CHANGELOG_FILE -m "Updated to version $NEXT for next development cycle"
 
 echo "--------------------------------------------------------------"
 echo "Released version $RELEASE and prepare $NEXT development cycle."
