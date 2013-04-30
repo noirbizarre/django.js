b80491c9c8c8f082bb6866e022e4a31fb2216082
Axel Haustant <noirbizarre@gmail.com>
2013-04-30 11:33:45 +0200

    Refactor wrapped lazy Django settings

--- djangojs/conf.py
+++ djangojs/conf.py
@@ -3,15 +3,14 @@ from __future__ import unicode_literals
 
 import sys
 
-from django.test.signals import setting_changed
-from django.dispatch import receiver
+from django.conf import settings as _settings
 
 from djangojs import JQUERY_DEFAULT_VERSION
 
 # Default configuration values for Django.js
 # All values used by Django.js needs to appears here.
 DEFAULTS = {
-    'DEBUG': True,
+    'DEBUG': False,
     'TESTING': 'test' in sys.argv,
     'JS_URLS': None,
     'JS_URLS_EXCLUDE': None,
@@ -23,46 +22,20 @@ DEFAULTS = {
     'JQUERY_VERSION': JQUERY_DEFAULT_VERSION,
 }
 
-# All required settings keys to import rom project
-REQUIREDS = (
-    'INSTALLED_APPS',
-    'STATIC_URL',
-    'ROOT_URLCONF',
-)
 
-
-class Settings(dict):
+class DjangoJsSettings(object):
     '''
-    Django settings wrapper
+    Lazy Django settings wrapper for Django.js
     '''
+    def __init__(self, wrapped_settings):
+        self.wrapped_settings = wrapped_settings
 
-    def __init__(self):
-        """
-        Try and initialize with Django settings.
-        All values from DEFAULTS will be overwritten by project settings if present.
-        """
-        try:
-            from django.conf import settings
+    def __getattr__(self, name):
+        if hasattr(self.wrapped_settings, name):
+            return getattr(self.wrapped_settings, name)
+        elif name in DEFAULTS:
+            return DEFAULTS[name]
+        else:
+            raise AttributeError("'%s' setting not found" % name)
 
-            self.update(DEFAULTS)
-            self.update(settings._wrapped.__dict__)
-
-            # Warn for missings settings
-            for key in REQUIREDS:
-                if not key in self:
-                    raise Exception('%s settings key is mandatory' % key)
-
-        except ImportError:
-            pass
-
-    def __getattr__(self, k):
-        return self[k]
-
-
-settings = Settings()
-
-
-@receiver(setting_changed)
-def on_settings_changed(sender, **kwargs):
-    key, value = kwargs['setting'], kwargs['value']
-    settings[key] = value
+settings = DjangoJsSettings(_settings)
